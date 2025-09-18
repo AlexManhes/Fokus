a6ca5b03f0c0e9b70040a685e1eb8102e7e89563
Alex Sander <92828095+AlexManhes@users.noreply.github.com>
2025-09-18 11:58:46 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -12,19 +12,36 @@ const duracaoDescansoCurto = 300;
 const duracaoDescansoLongo = 900;
 
 focoBt.addEventListener("click", (event) => {
-  html.setAttribute("data-contexto", "foco");
-  banner.setAttribute("src", "./imagens/foco.png");
+	alterarContexto("foco");
 });
 
 curtoBt.addEventListener("click", () => {
-  alterarContexto("curto");
+	alterarContexto("descanso-curto");
 });
 
 longoBt.addEventListener("click", (event) => {
-  html.setAttribute("data-contexto", "descanso-longo");
-  banner.setAttribute("src", "./imagens/descanso-longo.png");
+	alterarContexto("descanso-longo");
 });
 function alterarContexto(contexto) {
-  html.setAttribute("data-contexto", contexto);
-  banner.setAttribute("src", `./imagens/${contexto}.png`);
+	html.setAttribute("data-contexto", contexto);
+	banner.setAttribute("src", `./imagens/${contexto}.png`);
+	html.setAttribute("app__card-button"), "active";
+	switch (contexto) {
+		case "foco":
+			titulo.innerHTML = `  Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+
+			break;
+		case "descanso-curto":
+			titulo.innerHTML = `Que tal dar uma respirada?<br> 
+        <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+			break;
+		case "descanso-longa":
+			titulo.innerHTML = `Hora de voltar à superfície.<br> 
+        <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+			break;
+
+		default:
+			break;
+	}
 }
